d07e2b1488db03239d1b10b4584c95f2f9ab2de2
paultompkins <paultompkinselectrical@gmail.com>
2020-03-03 21:14:39 +1030

    my first commit

---
+++ hello.c
@@ -0,0 +1,25 @@
+//hello world in VS CODE
+
+//the following line is used to 
+//include the standard io library
+#include <stdio.h>
+
+//you ned a main function  for a 
+//program to complie
+int main(){
+//the following line 
+//prints the text to screen
+printf("hello World! ");
+printf("this is a simple program\n");
+printf("I'm pleased to meet you Robert +-)\n");
+
+/*
+this is the link to shortcuts in VS code
+https://code.visualstudio.com/shortcuts/keyboard-shortcuts-linux.pdf
+
+ssh url for git repo
+git@github.com:paultompkins/hello-world.git
+
+*/
+return (0);
+}
