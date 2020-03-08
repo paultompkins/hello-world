875fd77d6d9479c408330011b40259ee46a3bddf
paultompkins <paultompkinselectrical@gmail.com>
2020-03-09 08:22:32 +1030

    added url for wrecked

--- hello.c
+++ hello.c
@@ -20,6 +20,8 @@ https://code.visualstudio.com/shortcuts/keyboard-shortcuts-linux.pdf
 ssh url for git repo
 git@github.com:paultompkins/hello-world.git
 
+wrecked the game
+https://archive.org/details/msdos_Wrecked_-_A_Psychedelic_Adventure_1993
 */
 return (0);
 }
